5480d2418642136df5a5b4871fbc72b38511f917
落秋枫叶 <59348827+lqfy-jhc@users.noreply.github.com>
2023-09-30 01:22:27 +0800

    Update fileLink.bat
    已修正，绝对路径某文件夹中带有空格，导致生成失败
    提升，链接文件都将生成在脚本所在目录（c盘将能直接生成在脚本所在地，非c盘需要将%userprofile%\Desktop\LinkBox\目录下生成的脚本文件拖到需要的目标目录，右键管理员运行后将在此脚本目录产生链接文件）

--- 赢11/fileLink.bat
+++ 赢11/fileLink.bat
@@ -2,29 +2,37 @@
 set attribute="%~a1"
 set flieAttribute="--a--------"
 set directoryAttribute="d----------"
-set CLinkBoxPath=%userprofile%\Desktop\LinkBox\
+::rem 设定存放脚本及映射的目录
+set mappingPath=%userprofile%\Desktop\LinkBox\
+set CLinkBoxPath=%mappingPath%%~nx1
+::rem 脚本所在地即为链接生成地
+set tagHome="%~dp0"
 set OLinkBoxPath=%~d1\LinkBox\
 set Obat=%CLinkBoxPath%跨盘符需管理员权限%~n1.bat
-if exist %CLinkBoxPath% ( echo "%CLinkBoxPath%%~nx1" ) else ( mkdir %CLinkBoxPath% )
-::echo %attribute%
-::echo %flieAttribute%
-::echo %directoryAttribute%
+::rem 创建脚本及映射目录
+if exist %CLinkBoxPath% ( echo "%CLinkBoxPath%" ) else ( mkdir %CLinkBoxPath% )
+::rem 核验变量
+echo attribute：%attribute%
+::echo flieAttribute：%flieAttribute%
+::echo directoryAttribute：%directoryAttribute%
+
+::rem 如果文件在c盘
 if "C:"=="%~d1" ( goto intoC ) else ( goto intoO )
 
 :intoC
     if %flieAttribute%==%attribute% (
-        MKLINK /H "%CLinkBoxPath%%~nx1" "%1"
+        MKLINK /H "%tagHome%" "%~1"
         echo "--a--------H"
     ) else if %directoryAttribute%==%attribute% (
-        MKLINK /J "%CLinkBoxPath%%~nx1" "%1"
+        MKLINK /J "%tagHome%" "%~1"
         echo "d----------J"
     ) else (
-        MKLINK /D "%CLinkBoxPath%%~nx1" "%1"
+        MKLINK /D "%tagHome%" "%~1"
         echo "-----------D"
     )
     goto end
 
-
+::rem 如果文件在非c盘
 :intoO
     ::if exist %OLinkBoxPath% (
     ::    echo "%OLinkBoxPath%%~nx1"
@@ -37,17 +45,17 @@ if "C:"=="%~d1" ( goto intoC ) else ( goto intoO )
     ) > %Obat%
     if %flieAttribute%==%attribute% (
         (
-            echo MKLINK "%CLinkBoxPath%%~nx1" "%1"
+            echo MKLINK "%tagHome%" "%~1"
             echo echo --a--------H
         ) >> %Obat%
     ) else if %directoryAttribute%==%attribute% (
         (
-            echo MKLINK /D "%CLinkBoxPath%%~nx1" "%1"
+            echo MKLINK /D "%tagHome%" "%~1"
             echo echo d----------J
         ) >> %Obat%
     ) else (
         (
-            echo MKLINK /D "%CLinkBoxPath%%~nx1" "%1"
+            echo MKLINK /D "%tagHome%" "%~1"
             echo echo -----------D
         ) >> %Obat%
     )
